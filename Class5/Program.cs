9f40cd7643b9d9e4cd4ebb7b56f40357d312390a
a2n0g0e2l <125489061+a2n0g0e2l@users.noreply.github.com>
2023-02-23 11:50:14 -0600

    codigo finish

---
+++ Class5/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.Write("Ingrese el valor inicial: ");
+            int valorInicial = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el valor final: ");
+            int valorFinal = int.Parse(Console.ReadLine());
+
+            Console.Write("Los números pares dentro del rango ingresado son: ");
+
+            for (int i = valorInicial; i <= valorFinal; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
